fd965621a465a0ef098ed9960ed731b32869eaff
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 21:09:08 +0800

    Create minCoinChange.js

---
+++ algorithms/dynamic programming/minCoinChange.js
@@ -0,0 +1,36 @@
+function minCoinChange(coins, amount) {
+  const cache = [];
+
+  const makeChange = (value) => {
+    if(!value) {
+      return [];
+    }
+    if (cache[value]) {
+      return cache[value];
+    }
+    let min = [];
+    let newMin;
+    let newAmount;
+    for (let i = 0; i < coins.length; i++) {
+      const coin = coins[i];
+      newAmount = value - coin;
+     // console.log(coin);
+      if (newAmount >= 0) {
+        newMin  = makeChange(newAmount);
+      }
+      if (
+        newAmount >= 0 
+        && (newMin.length < min.length - 1 ||!min.length)
+        && (newMin.length || !newAmount)
+        ) {
+        min = [coin].concat(newMin);
+      console.log('new Min ' + min + ' for ' + amount);
+      }
+    }
+    return (cache[value] = min);
+  };
+  return makeChange(amount);
+}
+
+console.log(minCoinChange([1, 3, 4], 6));
+
