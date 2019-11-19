dde935f2271827290e92ecdf1e38a0947991e13c
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:40:26 +0800

    Create HashTable.js

---
+++ data_structures/dictionary/HashTable.js
@@ -0,0 +1,105 @@
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+function defaultToString(item) {
+  if (item === null) {
+    return 'NULL';
+  } if (item === undefined) {
+    return 'UNDEFINED';
+  } if (typeof item === 'string' || item instanceof String) {
+    return `${item}`;
+  }
+  return item.toString();
+}
+
+class HashTable {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+  put(key,value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      this.table[position] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+  get(key) {
+    const valuepair = this.table[this.hashCode(key)];
+    return valuepair == null ? undefined : valuepair.value;
+  }
+  remove(key) {
+    const hash = this.hashCode(key);
+    const valuepair = this.table[hash];
+    if (valuepair != null ) {
+      delete this.table[hash];
+      return true;
+    }
+    return false;
+  }
+    getTable() {
+    return this.table;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const keys = Object.keys(this.table);
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
+    for (let i = 1; i < keys.length; i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`;
+    }
+    return objString;
+  }
+
+
+
+}
+
+const hash = new HashTable();
+console.log(hash.hashCode('Bob') + '-Bob');
+hash.put('Bob', 20); 
+console.log(hash);
+hash.put('Alice', 21); 
+hash.put('Jack', 19); 
+console.log(hash);
+console.log(hash.get('Alice'));
+hash.remove('Jack');
+console.log(hash);
+
