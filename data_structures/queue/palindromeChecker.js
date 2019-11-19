cb0438abf3bc5fa8b9b154d0598cfc56542bc1cd
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:30:44 +0800

    Create palindromeChecker.js

---
+++ data_structures/queue/palindromeChecker.js
@@ -0,0 +1,34 @@
+function palindromeChecker(aString){
+if (
+aString === undefined 
+|| aString === null 
+|| (aString !== null && aString.length === 0)
+) {
+return false;
+}
+const deque = new Deque;
+const lowerString = aString.toLocaleLowerCase().split(' ').join('');
+let firstChar;
+let lastChar;
+
+for (let i = 0; i <lowerString.length; i++){
+deque.addBack(lowerString.charAt(i));
+}
+while (deque.size() > 1){
+firstChar = deque.removeFront();
+lastChar = deque.removeBack();
+if (firstChar !== lastChar) {
+return false;
+}
+}
+return true;
+}
+console.log(palindromeChecker(''))
+console.log(palindromeChecker('a'))
+console.log(palindromeChecker(' a'))
+console.log(palindromeChecker('aba'))
+console.log(palindromeChecker('12a21'))
+
+ 
+
+palindromeChecker
