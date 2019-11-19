e790ff5198f18da71cc0e2b3f60df5b881710ace
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:29:51 +0800

    Create Deque.js

---
+++ data_structures/queue/Deque.js
@@ -0,0 +1,97 @@
+class Deque {
+constructor() {
+this.count = 0;
+this.lowestCount = 0;
+this.items = {};
+}
+addFront(element){
+if (this.isEmpty()){
+this.addBack(element);
+} else if (this.lowestCount > 0){
+this.lowestCount--;
+this.items[this.lowestCount] = element;
+} else {
+for (let i = this.count; i >0 ; i--){
+this.items[i] = this.items[i-1];
+}
+this.count++;
+this.items[0] = element;
+}
+}
+addBack(element) {
+this.items[this.count] = element;
+this.count++;
+}
+
+removeFront() {
+if (this.isEmpty()) {
+return undefined;
+}
+const result = this.items[this.lowestCount];
+delete this.items[this.lowestCount];
+this.lowestCount++;
+return result;
+}
+removeBack() {
+if (this.isEmpty()) {
+return undefined;
+}
+this.count--;
+const result = this.items[this.count];
+delete this.items[this.count];
+return result;
+}
+
+peekFront() {
+if (this.isEmpty()) {
+return undefined;
+}
+return this.items[this.lowestCount];
+}
+peekBack() {
+if (this.isEmpty()) {
+return undefined;
+}
+return this.items[this.count - 1];
+}
+isEmpty() {
+return this.size() === 0;
+}
+
+clear() {
+this.items = {};
+this.count = 0;
+this.lowestCount = 0;
+}
+
+size() {
+return this.count - this.lowestCount;
+}
+
+toString() {
+if (this.isEmpty()) {
+return '';
+}
+let objString = `${this.items[this.lowestCount]}`;
+for (let i = this.lowestCount + 1; i < this.count; i++) {
+objString = `${objString},${this.items[i]}`;
+}
+return objString;
+}
+}
+
+const deque = new Deque();
+console.log(deque.isEmpty());
+deque.addBack('a');
+deque.addBack('b');
+console.log(deque);
+console.log(deque.toString());
+deque.addBack('c');
+deque.removeFront();
+console.log(deque);
+deque.removeBack();
+console.log(deque);
+deque.addFront('d');
+console.log(deque);
+
+Deque
