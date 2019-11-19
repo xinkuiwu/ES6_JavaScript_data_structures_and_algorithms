64fa7c244cb4a30c1adaa633409623a7e01f47e7
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 21:10:26 +0800

    Create floydWarshall.js

---
+++ algorithms/dynamic programming/floydWarshall.js
@@ -0,0 +1,53 @@
+const floydWarshall = graph => {
+  const dist = [];
+  const {length} = graph;
+  for (let i = 0; i < length; i++) {
+    dist[i] = [];
+    for (let j = 0; j < length; j++) {
+      if (i === j) {
+        dist[i][j] = 0;
+      } else if (!isFinite(graph[i][j])) {
+        dist[i][j] = Infinity;
+      } else {
+        dist[i][j] = graph[i][j];
+      }
+    }
+  }
+  for (let k = 0; k < length; k++) {
+    for(let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        if (dist[i][k] + dist[k][j] < dist[i][j]) {
+          dist[i][j] = dist[i][k] + dist[k][j];
+        }
+      }
+    }
+  }
+  return dist;
+};
+
+const INF = Infinity;
+const graph = [
+  [INF, 2, 4, INF, INF, INF],
+  [INF, INF, 2, 4, 2, INF],
+  [INF, INF, INF, INF, 3, INF],
+  [INF, INF, INF, INF, INF, 2],
+  [INF, INF, INF, 3, INF, 2],
+  [INF, INF, INF, INF, INF, INF]
+];
+
+dist = floydWarshall(graph);
+let s = '';
+for (let i = 0; i < dist.length; i++) {
+  s = '';
+  for (let j = 0; j < dist.length; j++) {
+    if (dist[i][j] === INF) {
+      s += 'INF ';
+    } else {
+      s += dist[i][j] + ' ';
+    }
+   
+  }
+  console.log(s);
+}
+
+
