af3064a667ae3c32dc5dc5246143b74a91d43459
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:35:37 +0800

    Create SortedLinkedList.js

---
+++ data_structures/linkedList/SortedLinkedList.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(element, next) {
+    this.element = element;
+    this.next = next;
+  }
+}
+
+function defaultEquals(a, b) {
+  return a === b;
+}
+
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+class SortedLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals, compareFn = defaultCompare) {
+    super(equalsFn);
+    this.equalsFn = equalsFn;
+    this.compareFn = compareFn;
+  }
+  push(element) {
+    if (this.isEmpty()) {
+      super.push(element);
+    } else {
+      const index = this.getIndexNextSortedElement(element);
+      super.insert(element, index);
+    }
+  }
+  insert(element, index = 0) {
+    if (this.isEmpty()) {
+      return super.insert(element, index === 0 ? index : 0);
+    }
+    const pos = this.getIndexNextSortedElement(element);
+    return super.insert(element, pos);
+  }
+  getIndexNextSortedElement(element) {
+    let current = this.head;
+    let i = 0;
+    for (; i < this.size() && current; i++) {
+      const comp = this.compareFn(element, current.element);
+      if (comp === Compare.LESS_THAN) {
+        return i;
+      }
+      current = current.next;
+    }
+    return i;
+  }
+}
+
+const list = new SortedLinkedList();
+for (let i = 5; i > 0; i--) {
+  list.push(i);
+}
+console.log(list);
+list.insert(1);
+console.log(list);
+
