e4c1c5265fddbf9b238e06ce82f8d5d256bc0cfb
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:50:22 +0800

    Create MaxHeap.js

---
+++ data_structures/heap/MaxHeap.js
@@ -0,0 +1,8 @@
+class MaxHeap extends MinHeap {
+  constructor(compareFn = defaultCompare) {
+    super(compareFn);
+    this.compareFn = compareFn;
+    this.compareFn = reverseCompare(compareFn);
+  }
+
+MaxHeap
