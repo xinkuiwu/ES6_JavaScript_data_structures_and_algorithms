3e81a77058d9b071a77706f7b5a3f0ed3e5a49ef
xinkuiwu <30287306+xinkuiwu@users.noreply.github.com>
2019-11-19 20:58:13 +0800

    Create mergeSort.js

---
+++ algorithms/sorting/mergeSort.js
@@ -0,0 +1,50 @@
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function swap(array, a, b) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+function merge(left, right, compareFn) {
+  let i = 0;
+  let j = 0;
+  const result = [];
+  while (i < left.length && j < right.length) {
+    result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]);
+
+  }
+  return result.concat(i < left.length ? left.slice(i) : right.slice(j));
+}
+
+function mergeSort(array, compareFn = defaultCompare) {
+  if (array.length > 1) {
+    const {length} = array;
+    const middle = Math.floor(length / 2);
+    const left = mergeSort(array.slice(0, middle), compareFn);
+    const right = mergeSort(array.slice(middle, length), compareFn);
+    array = merge(left, right, compareFn);
+  }
+  return array;
+}
+
+
+
+
+
+const array = [8,7,6,5,4,3,2,1];
+console.log(array);
+console.log(mergeSort(array));
+
